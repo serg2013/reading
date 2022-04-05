c603ce2a96f06508fbb98c3368d79495eb2de1c4
serg2013 <raven1901@mail.ru>
2022-04-05 19:44:19 +0300

    nc

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -12,10 +12,11 @@ func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "allowedHeaders")
-		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		//w.Header().Set("Access-Control-Allow-Headers", "allowedHeaders")
+		//w.Header().Set("Access-Control-Expose-Headers", "Authorization")
 		next(w, r)
 	}
 }
--- api/server.go
+++ api/server.go
@@ -15,7 +15,7 @@ var server = controllers.Server{}
 func init() {
 	// loads values from .env into the system
 	if err := godotenv.Load(); err != nil {
-		log.Print("sad .env file found")
+		log.Print("bad .env file found")
 	}
 }
 
